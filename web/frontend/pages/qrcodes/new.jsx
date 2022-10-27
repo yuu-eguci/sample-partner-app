f3202df447dcc3fb85d8d32c45b4093d0770dbac
yuu-eguci <yuu.eguci@gmail.com>
2022-10-27 13:53:52 +0900

    ✨ web/frontend/pages/qrcodes/new.jsx をドキュメント通り作成

---
+++ web/frontend/pages/qrcodes/new.jsx
@@ -0,0 +1,20 @@
+// NOTE: Getting started の通りに丸ごとコピペしたもの。
+//       https://shopify.dev/apps/getting-started/build-app-example/frontend
+import { Page } from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { QRCodeForm } from "../../components";
+
+export default function ManageCode() {
+  const breadcrumbs = [{ content: "QR codes", url: "/" }];
+
+  return (
+    <Page>
+      <TitleBar
+        title="Create new QR code"
+        breadcrumbs={breadcrumbs}
+        primaryAction={null}
+      />
+      <QRCodeForm />
+    </Page>
+  );
+}
